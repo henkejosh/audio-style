e2a7c620c7830b7da22f58b8a092e391342ae765
Josh Henke <joshhenke@Joshs-MacBook-Air.local>
2016-06-28 15:50:59 -0700

    Added User Model and controller

---
+++ frontend/soundClone.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// document.addEventListener("DOMContentLoaded",
+//   ReactDOM.render(<div>Loading</div>, document.getElementById("root"))
+// );
