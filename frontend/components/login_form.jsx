5cdeb426dc64a03d2c9c384311d3d6ad75386f90
Josh Henke <joshhenke@Joshs-MacBook-Air.local>
2016-06-29 15:51:42 -0700

    building out login_form

--- frontend/components/login_form.jsx
+++ frontend/components/login_form.jsx
@@ -7,25 +7,40 @@ const LoginForm = React.createClass({
     return { email: "", password: "" }
   },
 
+  componentDidMount: function() {
+    SessionStore.addListener(this.isUserLoggedIn);
+  },
+
+  isUserLoggedIn: function() {
+    if(SessionStore.isUserLoggedIn()) {
+      this.context.router.push("/");
+    }
+  },
+
   update: function(prop) {
     return (e) => {
       this.setState({
-        prop: e.target.value
+        [prop]: e.target.value
       });
     }
   },
 
+  loggingIn: function(event) {
+    event.preventDefault();
+    SessionActions.login(this.state);
+  },
+
   render: function() {
     return (
-      <form action="POST" onSubmit={SessionActions.login}>
+      <form onSubmit={this.loggingIn}>
       <label name="email">Email: </label>
       <input type="text" id="email" value={this.state.email}
         onChange={this.update("email")}></input>
-
+        <br/><br/>
       <label name="password">Password: </label>
       <input type="password" id="password" value={this.state.password}
         onChange={this.update("password")}></input>
-
+        <br/><br/>
       <input type="submit" value="Submit"></input>
       </form>
     );
