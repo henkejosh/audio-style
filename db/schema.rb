3b4a17cbf436c686cffffb7324735438bb97d905
Josh Henke <joshhenke@Joshs-MacBook-Air.local>
2016-06-29 10:07:06 -0700

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160628233017) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "password_digest", null: false
@@ -22,6 +25,6 @@ ActiveRecord::Schema.define(version: 20160628233017) do
     t.string   "profile_img_url"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
