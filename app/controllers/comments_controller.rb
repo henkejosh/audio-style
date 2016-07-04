1932232dd32ffe8a7e2be2e296ea6c138dfd6990
Josh Henke <henke.josh@gmail.com>
2016-07-03 17:56:38 -0700

    added CRUD functionality to Comment model/controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,47 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.find_by(params[:song_id])
+    render "api/comments/index"
+  end
+
+  def new
+		@comment = Comment.new
+		render json: @comment
+	end
+
+	def show
+		@comment = Comment.find(params[:id])
+		render "api/comments/show"
+	end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy!
+    render "api/comments"
+  end
+
+	def create
+		@comment = Comment.new(comment_params)
+
+		if @comment.save
+			render "api/comments/show"
+		else
+			render json: @comment.errors, status: 422
+		end
+	end
+
+	def update
+		@comment = Comment.find(params[:id])
+		@comment.update_attributes!(comment_params)
+		if @comment.save
+			render "api/comments/show"
+		else
+			render json: @comment.errors, status: 422
+		end
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:body, :user_id, :time_into_song, :song_id)
+	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ActiveRecord::Base
+  validates :body, :user_id, :song_id, presence: true
+
+  after_initialize :default_time_in
+
+  private
+  def default_time_in
+    self.time_into_song ||= 0
+  end
 end
